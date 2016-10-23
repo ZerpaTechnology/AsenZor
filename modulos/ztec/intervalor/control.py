1336cce6b734112b79af168309a016a41ef039d4
jesbram <jesus26abraham1996@gmail.com>
2016-10-23 12:04:03 -0400

    sigo trabajando en el modulo de intervalos, construidad unir All y otras funciones correjidas

--- modulos/ztec/intervalor/control.py
+++ modulos/ztec/intervalor/control.py
@@ -6,13 +6,24 @@ f=open("test.html","r")
 t=f.read()
 f.close()
 
-conjuntos,exclusiones=i.getConjuntos(t,["<div>","</div>"],["{{","}}"])
+#conjuntos,exclusiones=i.getConjuntos(t,["<div>","</div>"],["{{","}}"])
+#"hola mundo"
+# 0123456789
+#print conjuntos
+#print "conjuntos: ",conjuntos
+#print i.mostrarConjuntos(t,conjuntos)
+#print "---------------------------"
+#print "exclusiones: ",exclusiones
+#print i.mostrarConjuntos(t,exclusiones)
+#print ""
+#print "============================="
+#c=i.interInvAll(i.getInterval(t),exclusiones)
+#c=i.interInv([0,4],[4,10])
+#print c
+#print ""
+#print "#############################"
+#print i.mostrarConjuntos("hola mundo",c)
+#print i.unir([40,50],[20,30])
+#print i.seJunta([5,40],[20,30])
 
-print "conjuntos: ",conjuntos
-print i.mostrarConjuntos(t,conjuntos)
-print "---------------------------"
-print "exclusiones: ",exclusiones
-
-
-print i.mostrarConjuntos(t,exclusiones)
-
+print i.unirAll([5,500],[[100,150],[200,250],[300,310],[340,350]])
--- modulos/ztec/intervalor/intervalo.py
+++ modulos/ztec/intervalor/intervalo.py
@@ -7,6 +7,56 @@ def mostrarConjuntos(cadena,conjuntos):
 		l.append(cadena[elem[0]:elem[1]])
 	return l
 
+def getU(c1,c2):
+	if type(c1[0])==list or type(c1[0])==tuple:
+		if type(c2[0])==list or type(c2[0])==tuple:
+			if c1[0][0]>=c2[0][0]:
+				c3=[c1[0][0]]
+			else:
+				c3=[c2[0][0]]
+			if c1[-1][1]>=c2[-1][1]:
+				c3.append(c1[-1][1])
+			else:
+				c3.append(c2[-1][1])
+			return c3
+		else:
+			if c1[0][0]>=c2[0]:
+				c3=[c2[0]]
+			else:
+				c3=[c1[0][0]]
+			if c1[-1][1]>=c2[1]:
+				c3.append(c1[-1][1])
+			else:
+				c3.append(c2[1])
+		return c3
+	else:
+
+		if type(c2[0])==list or type(c2[0])==tuple:
+			if c1[0]>=c2[0][0]:
+				c3=[c1[0]]
+			else:
+				c3=[c2[0][0]]
+			if c1[-1]>=c2[-1][1]:
+				c3.append(c1[-1][1])
+			else:
+				c3.append(c2[-1][1])
+			return c3
+		else:
+
+			if c1[0]>=c2[0]:
+				c3=[c2[0]]
+
+			else:
+				c3=[c1[0]]
+			if c1[1]>=c2[1]:
+				c3.append(c1[1])
+			else:
+				c3.append(c2[1])
+			return c3
+
+						
+
+
 def getConjuntos(cadena,etiquetas,exclusion=None):
 	aperturas=[]
 	cierres=[]
@@ -16,7 +66,7 @@ def getConjuntos(cadena,etiquetas,exclusion=None):
 	ex_aper=[]
 	ex_cier=[]
 	exclusiones=[]
-	while c+len(etiquetas[1])<=len(cadena):
+	while c+len(etiquetas[1])<len(cadena):
 		if exclusion!=None:
 			if type(exclusion)==list:
 				if cadena[c:c+len(exclusion[0])]==exclusion[0]:
@@ -32,34 +82,117 @@ def getConjuntos(cadena,etiquetas,exclusion=None):
 		
 		if activa==False:
 			if cadena[c:c+len(etiquetas[0])]==etiquetas[0]:
-				aperturas.append(c)
+				aperturas=[c]
 			if cadena[c:c+len(etiquetas[1])]==etiquetas[1]:
-				cierres.append(c)
+				cierres=[c]
 		c+=1
 	
-	c=0
-	for elem in aperturas:
-		conjuntos.append([elem,cierres[c]])
-		c+=1
+	
 	c=0
 	for elem in ex_aper:
 		exclusiones.append([elem,ex_cier[c]])
 		c+=1
+	conjuntos=interInvAll([0,len(cadena)],exclusiones)
 	return [conjuntos,exclusiones]
 	
+def seJunta(c1,c2):
+	i1=c1[0]
+	i2=c2[0]
+	f1=c1[1]
+	f2=c2[1]
+	if i1<=i2 and f1<=f2 and f1>=i2:
+		return True
+	elif i2<=i1 and  i1<f1 and f2>=i1 and f2<=f1:
+		return True
+	else:
+		#seccion nueva
+		if i2<=i1 and f2>=f1 or i2>=i1 and f2<=f1:
+			return True
+		else:
+			return False
 
+#al parecer esta listo
+# c1 es un intervalo y c2 es una lista de intervalos
+def unirAll(c1,c2):
+	c=0
+	l=[c1]
 
-	
+	while c<len(c2):
+		if seJunta(c1,c2[c])==True:
+			l.extend([unir(l[-1],c2[c])])
+			del l[-2]
+			
+		else:
+			
+			if len(l)==1:
+				
+				l=unir(l[-1],c2[c])
+			else:
+				l.extend(unir(l[-1],c2[c]))
+				del l[-3]
+		
+		c+=1
+	return l
+
 def unir(c1,c2):
-	if c1[0]>=c2[0]:
-		c3=[c1[0]]
+	if type(c1[0])==list or type(c1[0])==tuple:
+		if type(c2[0])==list or type(c2[0])==tuple:
+			pass
+		else:
+			#si c1 es una lista y c2 no lo es
+			print "funcion no soportada haga paso a paso la union con unirAll"
+
 	else:
-		c3=[c2[0]]
-	if c1[1]>=c2[1]:
-		c3.append(c1[1])
-	else:
-		c3.append(c2[1])
-	return c3	
+		if type(c2[0])==list or type(c2[0])==tuple:
+			print "funcion no soportada haga paso a paso la union con unirAll"
+
+		else:
+			#seccion lista
+			i1=c1[0]
+			i2=c2[0]
+			f1=c1[1]
+			f2=c2[1]
+			#listo
+			#1
+			
+			
+			if i1<=i2 and f1<=f2 and f1>=i2:
+				if i1<=i2 and f1<=f2:
+					
+					c3=[i1]
+				else:
+					c3=[i2]
+
+				if f1>=f2 and i1>=i2:
+					c3.append(f1)
+				else:
+					c3.append(f2)
+				return c3
+			#listo
+			#2
+			elif i2<=i1 and  i1<f1 and f2>=i1 and f2<=f1:
+				
+				return [i2,f1]
+
+
+			else:
+				if i2<=i1 and f2>=f1 or i2>=i1 and f2<=f1:
+					#uno contien a otro
+					if i1<=i2 and f1>=f2:
+						return [i1,f1]
+					else:
+						return [i2,f2]
+				else:
+					#no se juntan
+					if i1>=i1 and f1>=f2:
+						return [c2,c1]
+					else:
+						return [c1,c2]
+
+
+
+#captura c1 en c2
+
 def inter(c1,c2):
 	if c1!=[]:
 		if c2!=[]:
@@ -88,21 +221,27 @@ def interList(c1,cs):
 	for elem in cs[0]:
 		c1=inter(c1,elem)
 	return c1
+#[cerrado,abierto]
+#borra c2 en c1
 def interInv(c1,c2):
-	if c1[0]<=c2[0]:
-		c3=[c1[0],c2[0]]
-	else:
-		c3=[c2[0],c1[0]]
+		if c1[0]<=c2[0]:
+			c3=[c1[0],c2[0]]
+		else:
+			c3=[c2[0],c1[0]]
 
-	if c1[1]>=c2[1]:
-		c4=[c2[1],c1[1]]
-	else:
-		c4=[c1[1],c2[1]]
-	return [c3,c4]
+		if c1[1]>=c2[1]:
+			c4=[c2[1],c1[1]]
+		else:
+			c4=[c1[1],c2[1]]
+		return [c3,c4]
 
 
-def interInvAll(c1,*cs):
+def interInvAll(c1,cs):
 	for elem in cs:
+		
 		c1=interInv(c1,elem)
+		
 	return c1
 
+def getInterval(string):
+	return[0,len(string)-1]
